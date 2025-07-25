dc1ca7faa5d3e1b19aff046690b5921bf1d26e45
Adarsh Kumar Dash <adarsh.dps.2020@gmail.com>
2025-07-26 01:10:57 +0530

    Updated app.py for Render deployment

--- app.py
+++ app.py
@@ -127,8 +127,12 @@ def result():
         winner = "It's a Tie!"
     return render_template('result.html', user_score=u, comp_score=c, winner=winner)
 
-if __name__=='__main__':
-    app.run(debug=True)
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host='0.0.0.0', port=port)
+
 
 
 
